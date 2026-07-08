4e007d8d98d1a7a076940e55bba3c66ea943615d
Thiago Pires <thiagopiress1234@gmail.com>
2026-07-08 15:09:49 -0300

    Adicionado o pacote de leitura de PDF

--- EmissaoCND/src/CNDRules/CNDRules.cs
+++ EmissaoCND/src/CNDRules/CNDRules.cs
@@ -6,7 +6,8 @@ namespace EmissaoCND.src.CNDRules
 {
     public class CNDRules
     {
-        public const string StatusDownloadConcluido = "Download concluído";
+        public const string StatusDownloadConcluidoCNDNegativa = "Download concluído de CND Negativa";
+        public const string StatusDownloadConcluidoCNDPositiva = "Download concluído de CND Positiva";
         public const string StatusErro = "Erro ao processar o download, por gentileza verificar manualmente";
 
     }
---
+++ EmissaoCND/src/Services/Pdf/PdfReaderService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmissaoCND.src.Services.Pdf
+{
+    public class PdfReaderService
+    {
+    }
+}
